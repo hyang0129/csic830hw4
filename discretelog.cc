5f1f9c5082dec114cbbcc7948e2d41990f01358c
hy3134 <hy3134@g.rit.edu>
2022-04-06 15:38:11 -0400

    add cuda test

--- discretelog.cc
+++ discretelog.cc
@@ -7,9 +7,43 @@
 using namespace std;
 #include<map>
 #include<cmath>
+#include <unordered_map>
+#include <algorithm>
+#include <math.h> 
 
 
+int solve(int a, int b, int m) {
+    a %= m, b %= m;
+    int k = 1, add = 0, g;
+    while ((g = __gcd(a, m)) > 1) {
+        if (b == k)
+            return add;
+        if (b % g)
+            return -1;
+        b /= g, m /= g, ++add;
+        k = (k * 1ll * a / g) % m;
+    }
 
+    int n = sqrt(m) + 1;
+    int an = 1;
+    for (int i = 0; i < n; ++i)
+        an = (an * 1ll * a) % m;
+
+    unordered_map<int, int> vals;
+    for (int q = 0, cur = b; q <= n; ++q) {
+        vals[cur] = q;
+        cur = (cur * 1ll * a) % m;
+    }
+
+    for (int p = 1, cur = k; p <= n; ++p) {
+        cur = (cur * 1ll * an) % m;
+        if (vals.count(cur)) {
+            int ans = n * p - vals[cur] + add;
+            return ans;
+        }
+    }
+    return -1;
+}
 
 
 int main(int argc,char** argv){
@@ -20,16 +54,18 @@ int main(int argc,char** argv){
 	if (fscanf(fin, "%lld%lld%lld", &A, &B, &M))
 	{}
 
-	long long x = 1;
-	for(long long i = 0;i < M;++i){
-		if(x == B){
-			cout << i << endl;
-			fprintf(fout,"%lld\n",i);
-			break;
-		}else{
-			x = x * A % M;
-		}
-	}
+	//long long x = 1;
+	//for(long long i = 0;i < M;++i){
+	//	if(x == B){
+	//		cout << i << endl;
+	//		fprintf(fout,"%lld\n",i);
+	//		break;
+	//	}else{
+	//		x = x * A % M;
+	//	}
+	//}
+
+    fprintf(fout, "%lld\n", solve(A, B, M));
 
 	fclose(fin);
 	fclose(fout);
