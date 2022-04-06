7491b54b6289ed08e6c2ff956d0c95c5e6defa57
hy3134 <hy3134@g.rit.edu>
2022-04-06 12:13:34 -0400

    add cuda test

--- discretelog.cu
+++ discretelog.cu
@@ -1,71 +1,57 @@
-#include<stdio.h>
-#include<string.h>
-#include<algorithm>
-#include<queue>
-#include<vector>
-#include<iostream>
-using namespace std;
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
-static const int wholeArraySize = 100000000;
-static const int blockSize = 1024;
-static const int gridSize = 24;
+#include <stdio.h>
 
-__device__ bool lastBlock(int* counter) {
-    __threadfence(); //ensure that partial result is visible by all blocks
-    int last = 0;
-    if (threadIdx.x == 0)
-        last = atomicAdd(counter, 1);
-    return __syncthreads_or(last == gridDim.x - 1);
-}
-
-__global__ void sumCommMultiBlock(const int* gArr, int arraySize, int* gOut, int* lastBlockCounter) {
-    int thIdx = threadIdx.x;
-    int gthIdx = thIdx + blockIdx.x * blockSize;
-    const int gridSize = blockSize * gridDim.x;
-    int sum = 0;
-    for (int i = gthIdx; i < arraySize; i += gridSize)
-        sum += gArr[i];
-    __shared__ int shArr[blockSize];
-    shArr[thIdx] = sum;
-    __syncthreads();
-    for (int size = blockSize / 2; size > 0; size /= 2) { //uniform
-        if (thIdx < size)
-            shArr[thIdx] += shArr[thIdx + size];
-        __syncthreads();
-    }
-    if (thIdx == 0)
-        gOut[blockIdx.x] = shArr[0];
-    if (lastBlock(lastBlockCounter)) {
-        shArr[thIdx] = thIdx < gridSize ? gOut[thIdx] : 0;
-        __syncthreads();
-        for (int size = blockSize / 2; size > 0; size /= 2) { //uniform
-            if (thIdx < size)
-                shArr[thIdx] += shArr[thIdx + size];
-            __syncthreads();
-        }
-        if (thIdx == 0)
-            gOut[0] = shArr[0];
+__global__ void addKernel(int* c, const int* a, const int* b, int size) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size) {
+        c[i] = a[i] + b[i];
     }
 }
 
-__host__ int sumArray(int* arr) {
-    int* dev_arr;
-    cudaMalloc((void**)&dev_arr, wholeArraySize * sizeof(int));
-    cudaMemcpy(dev_arr, arr, wholeArraySize * sizeof(int), cudaMemcpyHostToDevice);
+// Helper function for using CUDA to add vectors in parallel.
+void addWithCuda(int* c, const int* a, const int* b, int size) {
+    int* dev_a = nullptr;
+    int* dev_b = nullptr;
+    int* dev_c = nullptr;
 
-    int out;
-    int* dev_out;
-    cudaMalloc((void**)&dev_out, sizeof(int) * gridSize);
+    // Allocate GPU buffers for three vectors (two input, one output)
+    cudaMalloc((void**)&dev_c, size * sizeof(int));
+    cudaMalloc((void**)&dev_a, size * sizeof(int));
+    cudaMalloc((void**)&dev_b, size * sizeof(int));
 
-    int* dev_lastBlockCounter;
-    cudaMalloc((void**)&dev_lastBlockCounter, sizeof(int));
-    cudaMemset(dev_lastBlockCounter, 0, sizeof(int));
+    // Copy input vectors from host memory to GPU buffers.
+    cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
 
-    sumCommMultiBlock << <gridSize, blockSize >> > (dev_arr, wholeArraySize, dev_out, dev_lastBlockCounter);
+    // Launch a kernel on the GPU with one thread for each element.
+    // 2 is number of computational blocks and (size + 1) / 2 is a number of threads in a block
+    addKernel << <2, (size + 1) / 2 >> > (dev_c, dev_a, dev_b, size);
+
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
     cudaDeviceSynchronize();
 
-    cudaMemcpy(&out, dev_out, sizeof(int), cudaMemcpyDeviceToHost);
-    cudaFree(dev_arr);
-    cudaFree(dev_out);
-    return out;
+    // Copy output vector from GPU buffer to host memory.
+    cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+}
+
+int main(int argc, char** argv) {
+    const int arraySize = 5;
+    const int a[arraySize] = { 1,  2,  3,  4,  5 };
+    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    int c[arraySize] = { 0 };
+
+    addWithCuda(c, a, b, arraySize);
+
+    printf("{1, 2, 3, 4, 5} + {10, 20, 30, 40, 50} = {%d, %d, %d, %d, %d}\n", c[0], c[1], c[2], c[3], c[4]);
+
+    cudaDeviceReset();
+
+    return 0;
 }
